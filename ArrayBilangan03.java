efb548e59c0bb6911bdf0afeb51662c578205c06
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-11-21 10:44:17 +0700

    ArrayBil 3

--- ArrayBilangan03.java
+++ ArrayBilangan03.java
@@ -1,16 +1,10 @@
-import java.util.Scanner;
-
 public class ArrayBilangan03 {
-
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
 
         int[] bil = {5, 13, -7, 17};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
