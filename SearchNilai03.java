9ffcb4f32b5c4ebdd1c9f14968cec14367e2b052
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-11-21 13:23:54 +0700

    Search 2

--- SearchNilai03.java
+++ SearchNilai03.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
 public class SearchNilai03 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("Masukkan banyak nilai yang ingin diinput :" );
+        int n = sc.nextInt();
+        int[] arrNilai = new int[n];
+
+        for(int i = 0; i < n; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+                arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+
         int hasil = 0;
 
         for(int i = 0; i < arrNilai.length; i++){
-            if(key == arrNilai[i]){
+            if(arrNilai[i] == key){
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
+
+        if (hasil == 0) {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        } else {
+            System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+(hasil+1));
+        }
     }
 }
